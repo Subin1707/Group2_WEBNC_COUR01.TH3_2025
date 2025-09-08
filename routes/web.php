9acca71e0ef46056b53d1ce3e55178f9c45d0762
Subin1707 <manhquyen2005xyz@gmail.com>
2025-09-08 10:15:52 +0700

    fix lỗi 404 not found

--- routes/web.php
+++ routes/web.php
@@ -7,25 +7,21 @@ use App\Http\Controllers\RoomController;
 use App\Http\Controllers\ShowtimeController;
 use App\Http\Controllers\TicketController;
 
-// Trang chủ -> điều hướng về movies
 Route::get('/', function () {
     return redirect()->route('movies.index');
 });
 
-// Group cho Admin Management
-Route::prefix('admin')->name('admin.')->group(function () {
-    // Quản lý phim
-    Route::resource('movies', MovieController::class);
+// Quản lý phim
+Route::resource('movies', MovieController::class);
 
-    // Quản lý rạp
-    Route::resource('theaters', TheaterController::class);
+// Quản lý rạp
+Route::resource('theaters', TheaterController::class);
 
-    // Quản lý phòng chiếu
-    Route::resource('rooms', RoomController::class);
+// Quản lý phòng chiếu
+Route::resource('rooms', RoomController::class);
 
-    // Quản lý suất chiếu
-    Route::resource('showtimes', ShowtimeController::class);
+// Quản lý suất chiếu
+Route::resource('showtimes', ShowtimeController::class);
 
-    // Quản lý vé
-    Route::resource('tickets', TicketController::class);
-});
+// Quản lý vé
+Route::resource('tickets', TicketController::class);
