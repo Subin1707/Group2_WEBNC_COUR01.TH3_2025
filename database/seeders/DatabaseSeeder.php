acb003043cd104fca08083504c6c80f183a49622
Subin1707 <manhquyen2005xyz@gmail.com>
2025-09-24 16:14:21 +0700

    thay đổi để lưu trữ

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Database\Factories\UserFactory;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,14 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
+            UserSeeder::class,      // chỉ seed admin
             TheaterSeeder::class,
             RoomSeeder::class,
             SeatSeeder::class,
             MovieSeeder::class,
             ShowtimeSeeder::class,
-            CustomerSeeder::class,
+            // ❌ KHÔNG gọi CustomerSeeder nữa
         ]);
     }
-
 }
